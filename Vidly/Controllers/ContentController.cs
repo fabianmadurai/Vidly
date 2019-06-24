065bc71a9618e724e222a544f688c6fab266c542
fabianmadurai <fabianmadurai@gmail.com>
2019-06-24 08:53:49 +0200

    Testing Commits. Added Content Controller.

---
+++ Vidly/Controllers/ContentController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        public ActionResult Index()
+        {
+            return Content("Hello");
+        }
+    }
+}
